fd6fe5877742d84807a983b1c2f27a1538c7cd3b
Lance Larsen <lance@lancelarsen.com>
2021-03-26 23:07:23 -0500

    Adding more positions

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/auto/runnable/Red1Left.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/auto/runnable/Red1Left.java
@@ -40,13 +40,16 @@ public class Red1Left extends LinearOpMode {
     }
     private enum Position
     {
+        S1W, S2W, S3W, S4W,
         R1, R2, R3, R4,
         X0, X1, X2,
-        P0, P1, P2,
+        L0, L1, L2,
         W1, W2, W3, W4, W5, W6,
         C1, C2,
         T1, T2, T3, T4, T5, T6,
-        Ps1, Ps2, Ps3, Ps4
+        P1A, P2A, P3A, P4A,
+        P1B, P2B, P3B, P4B,
+        P1C, P2C, P3C, P4C
     }
 
     private Pose2d _poseLast = null;
@@ -57,10 +60,10 @@ public class Red1Left extends LinearOpMode {
         double y = 0;
         switch(position) {
             case S0: x = 0;     y = 0;   break; //--- center of arena
-            case S1: x = -63.5; y = -36; break; //--- against wall, left blue
-            case S2: x = -63.5; y = -12; break; //--- against wall, right blue
-            case S3: x = -63.5; y = 12;  break; //--- against wall, left red
-            case S4: x = -63.5; y = 48;  break; //--- against wall, right red
+            case S1: x = -63.5; y = 47;  break; //--- against wall, left blue
+            case S2: x = -63.5; y = 11;  break; //--- against wall, right blue
+            case S3: x = -63.5; y = -16; break; //--- against wall, left red
+            case S4: x = -63.5; y = -61; break; //--- against wall, right red
             case S5: x = -63.5; y = 0;   break; //--- against wall, center
         }
 
@@ -96,6 +99,10 @@ public class Red1Left extends LinearOpMode {
         double x = 0;
         double y = 0;
         switch(position) {
+            case S1W: x = -61; y = 47;  break; //--- 2 inches away from wall, left blue
+            case S2W: x = -61; y = 11;  break; //--- 2 inches away from wall, right blue
+            case S3W: x = -61; y = -16; break; //--- 2 inches away from wall, left red
+            case S4W: x = -61; y = -61; break; //--- 2 inches away from wall, right red
             case R1: x = 0; y = 0; break;
             case R2: x = 0; y = 0; break;
             case R3: x = 0; y = 0; break;
@@ -103,10 +110,10 @@ public class Red1Left extends LinearOpMode {
             case X0: x = 0; y = 0; break; //--- center of the arena
             case X1: x = 0; y = 0; break;
             case X2: x = 0; y = 0; break;
-            case P0: x = 0; y = 0; break;
-            case P1: x = 0; y = 0; break;
-            case P2: x = 0; y = 0; break;
-            case W1: x = 0; y = 0; break;
+            case L0: x = 10; y = 0; break; //--- center on white line
+            case L1: x = 0; y = 0; break;
+            case L2: x = 0; y = 0; break;
+            case W1: x = 3; y = 60; break;
             case W2: x = 0; y = 0; break;
             case W3: x = 0; y = 0; break;
             case W4: x = 0; y = 0; break;
@@ -120,28 +127,33 @@ public class Red1Left extends LinearOpMode {
             case T4: x = 0; y = 0; break;
             case T5: x = 0; y = 0; break;
             case T6: x = 0; y = 0; break;
-            case Ps1: x = 0; y = 0; break;
-            case Ps2: x = 0; y = 0; break;
-            case Ps3: x = 0; y = 0; break;
-            case Ps4: x = 0; y = 0; break;
+            case P1A: x = 0; y = 0; break;
+            case P1B: x = 0; y = 0; break;
+            case P1C: x = 0; y = 0; break;
+            case P2A: x = -4; y = 11; break;
+            case P2B: x = -4; y = 4; break;
+            case P2C: x = -4; y = -3; break;
+            case P3A: x = 0; y = 0; break;
+            case P3B: x = 0; y = 0; break;
+            case P3C: x = 0; y = 0; break;
+            case P4A: x = 0; y = 0; break;
+            case P4B: x = 0; y = 0; break;
+            case P4C: x = 0; y = 0; break;
         }
 
         Trajectory trajectory = null;
         switch(type) {
             case Curve:
                 trajectory = _drive.trajectoryBuilder(_poseLast)
-                        .splineTo(new Vector2d(x, y), Math.toRadians(angle))
-                        .build();
+                    .splineTo(new Vector2d(x, y), Math.toRadians(angle)).build();
             break;
             case CurveFixedHeading:
                 trajectory = _drive.trajectoryBuilder(_poseLast)
-                        .splineToConstantHeading(new Vector2d(x, y), Math.toRadians(angle))
-                        .build();
+                    .splineToConstantHeading(new Vector2d(x, y), Math.toRadians(angle)).build();
                 break;
             case Straight:
                 trajectory = _drive.trajectoryBuilder(_poseLast)
-                        .lineToLinearHeading(new Pose2d(x, y, Math.toRadians(angle)))
-                        .build();
+                    .lineToLinearHeading(new Pose2d(x, y, Math.toRadians(angle))).build();
                 break;
         }
 
@@ -157,11 +169,22 @@ public class Red1Left extends LinearOpMode {
         _ringVision  = new RingVision(_opMode.hardwareMap);
         _drive = new BotMecanumDrive(hardwareMap);
 
-        SetStartPosition(StartingPosition.S5, 0);
+        SetStartPosition(StartingPosition.S2, 0);
 
         waitForStart();
         if (isStopRequested()) return;
 
-        DriveTo(Position.X0, 0, DriveType.Straight, Speed.Fast); //--- drive to center
+        DriveTo(Position.S2W, 0, DriveType.Straight, Speed.Slow); //--- drive away from wall
+        sleep(2000);
+        //DriveTo(Position.L0, 0, DriveType.Curve, Speed.Fast); //--- drive to middle of the white line
+
+        DriveTo(Position.P2A, 0, DriveType.Curve, Speed.Fast); //--- drive to power shot line
+        sleep(1000);
+        DriveTo(Position.P2B, 0, DriveType.Straight, Speed.Fast); //--- strafe
+        sleep(1000);
+        DriveTo(Position.P2C, 0, DriveType.Straight, Speed.Fast); //--- strafe
+
+        Turn(-90, Speed.Medium); //--- turn right 90 so we're lined up with wobble goal 1
+        DriveTo(Position.W1, 270, DriveType.Straight, Speed.Fast); //--- drive to wobble goal
     }
 }
